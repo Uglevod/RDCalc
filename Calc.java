3ab65fd7c5741333d86be5cf8f588f3efa8308e6
Ucl.org.ru <vauglev@gmail.com>
2019-07-23 23:42:21 +0700

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+ 
+
+/**
+ *
+ * @author uglevod
+ */
+public class Calc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        int[] ai=new int[2];
+       String out="";
+        
+        BufferedReader reader =  
+                   new BufferedReader(new InputStreamReader(System.in)); 
+        System.out.println("Enter line");
+        
+         
+       
+         String[] res=Check.ch(reader.readLine());
+       
+        
+         if (res[3]=="Rim") {
+         ai[0]=RomanToDecimal.romanToDecimal(res[0]);
+         ai[1]=RomanToDecimal.romanToDecimal(res[1]);
+         }
+         
+         if (res[3]=="Dec") {
+         ai[0]=Integer.parseInt(res[0]);
+         ai[1]=Integer.parseInt(res[1]);
+         }
+         
+         
+         
+         if(res[3]!="Err") { 
+          switch (res[2]) {
+                case "+":
+                    out=ai[0]+ai[1]+"";
+                    break;
+                case "-":
+                    out=ai[0]-ai[1]+"";
+                    break;
+                case "*":
+                    out=ai[0]*ai[1]+"";
+                    break;
+                case "/":
+                    out=ai[0]/ai[1]+"";
+                    break;
+          }
+         }
+          
+         if (res[3]=="Rim") {
+          
+         String gout=out;    
+         out=DecimalToRoman.toRoman( Integer.parseInt(gout)); 
+           
+         }
+         
+         
+        if (res[3]=="Err") {
+         //out="Ошибка ввода";
+            throw new IOException();
+         } 
+         
+         
+        System.out.println(out);         
+    }
+    
+}
---
+++ Check.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+
+/**
+ *
+ * @author uglevod
+ */
+  class  Check {
+    
+    
+    
+    public static String[] ch(String ln){
+      // переменная для хранения ответа  
+      String[] res = new String[4];
+        String s[];
+      // разделяем по оперратору  
+        s = ln.split("[-+*/]");
+      // если операндов больше чем 2 то ошибка  
+        if (s.length>2) {res[3]="Err";}
+      // проверка на односистемность
+      String rdec = "[0-9]+";
+      String rrim = "[IVXLCDM]+"; 
+      
+      if (s[0].trim().matches(rdec) && s[1].trim().matches(rdec) && res[3]!="Err") {res[3]="Dec"; }
+      else 
+          if ( s[0].trim().matches(rrim) && s[1].trim().matches(rrim)) {res[3]="Rim"; } 
+          else {res[3]="Err";}
+       
+      if (res[3]!="Err") {res[0]=s[0]; res[1]=s[1];}
+     //   System.out.println(ln.);
+      
+      if (ln.contains("+") && res[3]!="Err") {res[2]="+";}
+      if (ln.contains("-") && res[3]!="Err") {res[2]="-";}
+      if (ln.contains("*") && res[3]!="Err") {res[2]="*";}
+      if (ln.contains("/") && res[3]!="Err") {res[2]="/";}
+      
+      
+      
+      
+      
+      
+      return res;
+    
+    
+    } 
+}
